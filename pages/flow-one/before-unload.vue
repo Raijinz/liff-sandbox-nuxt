393bf4a356448c0b111cce9eb45ee34bca94f5e1
Kittitad Seelawiang <Raijinz@users.noreply.github.com>
2024-02-19 14:25:35 +0700

    Add beforeunload test page

---
+++ pages/flow-one/before-unload.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const beforeUnloadHandler = (event: Event) => {
+  event.preventDefault();
+};
+
+function nameInputHandler (event: Event) {
+    if ((event.target as HTMLInputElement).value !== "") {
+        onbeforeunload = beforeUnloadHandler;
+    } else {
+        onbeforeunload = null;
+    }
+};
+</script>
+
+<template>
+    <h1>Before unload page</h1>
+    <form>
+        <input type="text" name="name" id="name" @input="nameInputHandler" />
+    </form>
+</template>
+
+<style>
+
+</style>
